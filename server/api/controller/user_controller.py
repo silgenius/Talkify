f644cd3434e39a617c6212be023e341f0c0f4ed6
silgenius <olutademartin@gmail.com>
2024-08-26 22:29:31 +0000

    Fix route

--- server/api/controller/user_controller.py
+++ server/api/controller/user_controller.py
@@ -8,27 +8,18 @@ from server.models.user import User
 
 session = storage.get_session()
 
-@app_handler.route("/user/id", methods=['GET'])
-def user_by_id():
+@app_handler.route("/user/id/<string:user_id>", methods=['GET'])
+def user_by_id(user_id):
     """
     Retrieves a user based on their unique user ID
     """
-
-    try:
-        data = request.get_json()
-    except Exception:
-        return jsonify({"error": "Not a JSON"}), 400
-
-    user_id = data.get("id")
-    if not user_id:
-        return jsonify({"error": "Missing id"}), 400
 
     user = session.query(User).filter_by(id=user_id).one_or_none()
     if user:
         return jsonify(user.to_dict())
     abort(404)
 
-@app_handler.route("/user/email", methods=['GET'])
+@app_handler.route("/user/email", methods=['POST'])
 def user_by_email():
     """
     Retrieves a user based on their email address.
