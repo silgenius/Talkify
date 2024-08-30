3052a13c68467d4783af1ab024c2ac04dd795b34
silgenius <olutademartin@gmail.com>
2024-08-30 15:57:49 +0000

    Fix bug in socket

--- server/api/sockets/events/connection_event.py
+++ server/api/sockets/events/connection_event.py
@@ -2,24 +2,42 @@
 
 from flask_socketio import emit
 from server.api.sockets.events import socketio
+from server.models import storage
+from server.models.user import User
 
+session = storage.get_session()
 
 @socketio.on('conect_user')
 def handle_user_conected(data):
-    if not isinstace(data, dict):
+    print("connected before checking data check") #debugging
+    if not isinstance(data, dict):
         emit('error', {'error': 'data must be dict obj'})
+        return
 
     user_id = data.get("user_id")
-    user = session.query(User).filter_by(id=user_id).one_or_none() 
+    if not user_id:
+        emit('error', {'error': 'user id not found'})
+        return
+    user = session.query(User).filter_by(id=user_id).one_or_none()
+    if not user:
+        emit('error', {'error': 'user does not exist'})
+        return
     print("connected") #debugging
-    emit('user_connected', {'username': user.username, 'status': 'online'})
+    emit('user_connected', user.mini_data(), broadcast=True)
 
 @socketio.on('disconnect_user')
 def handle_user_disconnected(data):
-    if not isinstace(data, dict):
+    if not isinstance(data, dict):
         emit('error', {'error': 'data must be dict obj'})
+        return
 
     user_id = data.get("user_id")
+    if not user_id:
+         emit('error', {'error': 'user id not found'})
+         return
     user = session.query(User).filter_by(id=user_id).one_or_none()
+    if not user:
+        emit('error', {'error': 'user does not exist'})
+        return
     print("{} disconnected".format(user.username)) #debugging
-    emit('user_disconnected', {'username': user.username, 'status': 'offline'})
+    emit('user_disconnected', user.mini_data(), broadcast=True)
--- server/api/sockets/events/message_event.py
+++ server/api/sockets/events/message_event.py
@@ -4,51 +4,69 @@ from flask_socketio import emit
 from server.api.sockets.events import socketio
 from server.models.conversation import Conversation
 from server.models.message import Message
-
+from server.models import storage
+from server.models.user import User
 
 @socketio.on('send_message')
 def handle_message(data):
-    if not isinstace(data, dict):
+    if not isinstance(data, dict):
         emit('error', {'error': 'data must be dict obj'})
-    convo_id = data.get("conversation_id")
-    if not convo_id:
-        emit('error', {'error': 'conversatio_id missing'})
-    sender_id = data.get("sender_id")
-    if not sender_id:
-        emit('error', {'error': 'sender_id missing'})
-    message_type = data.get("message_type")
-    text = data.get("text")
-    conversation = session.query(Conversation).filter_by(id=conversation_id).one_or_none()
-    message = Message(conversation_id=conversation.id, sender_id=sender_id, message_text=text)
-    storage.new(message)
-    storage.save()
-    conversation.update_last_message_id(message.id)
-    emit('message_sent', conversation.to_dict(), broadcast=True)
+        return
+
+    message_id = data.get("message_id")
+    if not message_id:
+        emit('error', {'error': 'message_id missing'})
+        return
+
+    message = session.query(Message).filter_by(id=message_id).one_or_none()
+    if not message:
+        emit('error', {'error': 'message missing'})
+        return
+
+    emit('message_sent', message.to_dict(), broadcast=True)
 
 @socketio.on('start_typing')
 def handle_typing(data):
-    if not isinstace(data, dict):
+    if not isinstance(data, dict):
         emit('error', {'error': 'data must be dict obj'})
+        return
+
     convo_id = data.get("conversation_id")
     if not convo_id:
         emit('error', {'error': 'conversatio_id missing'})
+        return
+
     sender_id = data.get("sender_id")
     if not sender_id:
         emit('error', {'error': 'sender_id missing'})
+        return
 
     user = session.query(User).filter_by(id=sender_id).one_or_none()
+    if not user:
+        emit('error', {'error': 'user does not exist'})
+        return
+
     emit('typing_started', {'username': user.username, 'conversation_id': convo_id}, broadcast=True)
 
 @socketio.on('stop_typing')
 def handle_stop_typing(data):
-    if not isinstace(data, dict):
+    if not isinstance(data, dict):
         emit('error', {'error': 'data must be dict obj'})
+        return
+
     convo_id = data.get("conversation_id")
     if not convo_id:
         emit('error', {'error': 'conversatio_id missing'})
+        return
+
     sender_id = data.get("sender_id")
     if not sender_id:
         emit('error', {'error': 'sender_id missing'})
+        return
 
     user = session.query(User).filter_by(id=sender_id).one_or_none()
+    if not user:
+        emit('error', {'error': 'user does not exist'})
+        return
+
     emit('typing_stopped', {'username': user.username, 'conversation_id': convo_id}, broadcast=True)
