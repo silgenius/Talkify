91938b44a77b091e2f22bf559c701d5e651da720
silgenius <olutademartin@gmail.com>
2024-09-05 15:58:38 +0000

    Update Conversation api management

--- server/api/controller/__init__.py
+++ server/api/controller/__init__.py
@@ -8,3 +8,4 @@ app_handler = Blueprint('app_handler', __name__, url_prefix='/api')
 from server.api.controller.conversation_controller import *
 from server.api.controller.user_controller import *
 from server.api.controller.message_controller import *
+from server.api.controller.contact_controller import *
