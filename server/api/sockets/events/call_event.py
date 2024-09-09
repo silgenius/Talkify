a0c9ba8d31b183deca756405a5115617801323c3
silgenius <olutademartin@gmail.com>
2024-09-09 23:22:12 +0000

    Fix bug in events

--- server/api/sockets/events/call_event.py
+++ server/api/sockets/events/call_event.py
@@ -5,12 +5,12 @@ from server.api.sockets.events import socketio
 
 @socketio.on('initiate_call')
 def handle_call_init(data):
-    emit('call_initiated', data)
+    emit('call_initiated', data, broadcast=True)
 
 @socketio.on('accept_call')
 def handle_call_accept(data):
-    emit('call_accepted', data)
+    emit('call_accepted', data, broadcast=True)
 
 @socketio.on('end_call')
 def handle_call_end(data):
-    emit('call_ended', data)
+    emit('call_ended', data, broadcast=True)
