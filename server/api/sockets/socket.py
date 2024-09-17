421e3937da7b533867a02255e65920c6880df522
silgenius <olutademartin@gmail.com>
2024-09-17 20:51:34 +0000

    configure socket for production

--- server/api/sockets/socket.py
+++ server/api/sockets/socket.py
@@ -7,7 +7,6 @@
 from server.api.sockets.events import app, socketio
 import os
 
-print(os.getenv('SECRET_KEY'))
 
 if __name__ == '__main__':
-    socketio.run(app, port=5001, host = '0.0.0.0', debug=True)
+    socketio.run(app, port=5001, host = '0.0.0.0')
