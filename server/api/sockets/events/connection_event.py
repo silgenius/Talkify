23c4c8325cbdf83a146f33041cc3fa2806b78910
SeM2x <houssem135.302@gmail.com>
2024-09-19 22:04:52 +0100

    socket server bug fix

--- server/api/sockets/events/connection_event.py
+++ server/api/sockets/events/connection_event.py
@@ -5,40 +5,48 @@ from server.api.sockets.events import socketio
 from server.models import storage
 from server.models.user import User
 
-session = storage.get_session()
 
 @socketio.on('connect_user')
 def handle_user_conected(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    user_id = data.get("user_id")
-    if not user_id:
-        emit('error', {'error': 'user id not found'})
-        return
+        user_id = data.get("user_id")
+        if not user_id:
+            emit('error', {'error': 'user id not found'})
+            return
 
-    user = session.query(User).filter_by(id=user_id).one_or_none()
-    if not user:
-        emit('error', {'error': 'user does not exist'})
-        return
+        user = session.query(User).filter_by(id=user_id).one_or_none()
+        if not user:
+            emit('error', {'error': 'user does not exist'})
+            return
 
-    emit('user_connected', user.mini_data(), broadcast=True)
+        emit('user_connected', user.mini_data(), broadcast=True)
+    finally:
+        session.close()
+
 
 @socketio.on('disconnect_user')
 def handle_user_disconnected(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    user_id = data.get("user_id")
-    if not user_id:
-         emit('error', {'error': 'user id not found'})
-         return
+        user_id = data.get("user_id")
+        if not user_id:
+            emit('error', {'error': 'user id not found'})
+            return
 
-    user = session.query(User).filter_by(id=user_id).one_or_none()
-    if not user:
-        emit('error', {'error': 'user does not exist'})
-        return
+        user = session.query(User).filter_by(id=user_id).one_or_none()
+        if not user:
+            emit('error', {'error': 'user does not exist'})
+            return
 
-    emit('user_disconnected', user.mini_data(), broadcast=True)
+        emit('user_disconnected', user.mini_data(), broadcast=True)
+    finally:
+        session.close()
--- server/api/sockets/events/message_event.py
+++ server/api/sockets/events/message_event.py
@@ -8,116 +8,140 @@ from server.models import storage
 from server.models.user import User
 
 
-session = storage.get_session()
-
 @socketio.on('send_message')
 def handle_message(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    message_id = data.get("message_id")
-    if not message_id:
-        emit('error', {'error': 'message_id missing'})
-        return
+        message_id = data.get("message_id")
+        if not message_id:
+            emit('error', {'error': 'message_id missing'})
+            return
 
-    message = session.query(Message).filter_by(id=message_id).one_or_none()
-    if not message:
-        emit('error', {'error': 'message missing'})
-        return
+        message = session.query(Message).filter_by(id=message_id).one_or_none()
+        if not message:
+            emit('error', {'error': 'message missing'})
+            return
 
-    emit('message_sent', message.to_dict(), broadcast=True)
+        emit('message_sent', message.to_dict(), broadcast=True)
+    finally:
+        session.close()
+
 
 @socketio.on('start_typing')
 def handle_typing(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    convo_id = data.get("conversation_id")
-    if not convo_id:
-        emit('error', {'error': 'conversation id missing'})
-        return
+        convo_id = data.get("conversation_id")
+        if not convo_id:
+            emit('error', {'error': 'conversation id missing'})
+            return
 
-    sender_id = data.get("sender_id")
-    if not sender_id:
-        emit('error', {'error': 'sender_id missing'})
-        return
+        sender_id = data.get("sender_id")
+        if not sender_id:
+            emit('error', {'error': 'sender_id missing'})
+            return
 
-    user = session.query(User).filter_by(id=sender_id).one_or_none()
-    if not user:
-        emit('error', {'error': 'user does not exist'})
-        return
+        user = session.query(User).filter_by(id=sender_id).one_or_none()
+        if not user:
+            emit('error', {'error': 'user does not exist'})
+            return
 
-    emit('typing_started', {'username': user.username, 'conversation_id': convo_id}, broadcast=True)
+        emit('typing_started', {'username': user.username,
+             'conversation_id': convo_id}, broadcast=True)
+    finally:
+        session.close()
+
 
 @socketio.on('stop_typing')
 def handle_stop_typing(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    convo_id = data.get("conversation_id")
-    if not convo_id:
-        emit('error', {'error': 'conversatio_id missing'})
-        return
+        convo_id = data.get("conversation_id")
+        if not convo_id:
+            emit('error', {'error': 'conversatio_id missing'})
+            return
 
-    sender_id = data.get("sender_id")
-    if not sender_id:
-        emit('error', {'error': 'sender_id missing'})
-        return
+        sender_id = data.get("sender_id")
+        if not sender_id:
+            emit('error', {'error': 'sender_id missing'})
+            return
 
-    user = session.query(User).filter_by(id=sender_id).one_or_none()
-    if not user:
-        emit('error', {'error': 'user does not exist'})
-        return
+        user = session.query(User).filter_by(id=sender_id).one_or_none()
+        if not user:
+            emit('error', {'error': 'user does not exist'})
+            return
 
-    emit('typing_stopped', {'username': user.username, 'conversation_id': convo_id}, broadcast=True)
+        emit('typing_stopped', {'username': user.username,
+             'conversation_id': convo_id}, broadcast=True)
+    finally:
+        session.close()
+
 
 @socketio.on('deliver_message')
 def handle_delivered(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    message_id = data.get("message_id")
-    if not message_id:
-        emit('error', {'error': 'message_id missing'})
-        return
+        message_id = data.get("message_id")
+        if not message_id:
+            emit('error', {'error': 'message_id missing'})
+            return
 
-    message = session.query(Message).filter_by(id=message_id).one_or_none()
-    if not message:
-        emit('error', {'error': 'message missing'})
-        return
+        message = session.query(Message).filter_by(id=message_id).one_or_none()
+        if not message:
+            emit('error', {'error': 'message missing'})
+            return
 
-    message.status = MessageStatus.delivered
-    storage.save()
+        message.status = MessageStatus.delivered
+        storage.save()
 
-    msg = message.to_dict()
-    msg.pop("message_text")
-    msg.pop("message_type")
-    emit('message_delivered', msg, broadcast=True)
+        msg = message.to_dict()
+        msg.pop("message_text")
+        msg.pop("message_type")
+        emit('message_delivered', msg, broadcast=True)
+    finally:
+        session.close()
+
 
 @socketio.on('read_message')
 def handle_read_message(data):
-    if not isinstance(data, dict):
-        emit('error', {'error': 'data must be dict obj'})
-        return
+    session = storage.get_session()
+    try:
+        if not isinstance(data, dict):
+            emit('error', {'error': 'data must be dict obj'})
+            return
 
-    message_id = data.get("message_id")
-    if not message_id:
-        emit('error', {'error': 'message_id missing'})
-        return
+        message_id = data.get("message_id")
+        if not message_id:
+            emit('error', {'error': 'message_id missing'})
+            return
 
-    message = session.query(Message).filter_by(id=message_id).one_or_none()
-    if not message:
-        emit('error', {'error': 'message missing'})
-        return
+        message = session.query(Message).filter_by(id=message_id).one_or_none()
+        if not message:
+            emit('error', {'error': 'message missing'})
+            return
 
-    message.status = MessageStatus.seen
-    storage.save()
+        message.status = MessageStatus.seen
+        storage.save()
 
-    msg = message.to_dict()
-    msg.pop("message_text")
-    msg.pop("message_type")
-    emit('message_read', msg, broadcast=True)
+        msg = message.to_dict()
+        msg.pop("message_text")
+        msg.pop("message_type")
+        emit('message_read', msg, broadcast=True)
+    finally:
+        session.close()
